771b6dde515ec3176ac6302f9e31487e09378970
Filippo <github@filippo.pw>
2017-10-20 22:18:42 +0200

    Create vimuxpy.sh

---
+++ vimuxpy.sh
@@ -0,0 +1,26 @@
+###Starts vim inside a split tmux window
+
+exec 2>/dev/null
+
+function start_tmux {
+	fbname=$(basename "$1" | cut -d. -f1)	
+	if `tmux has-session -t $fbname`; then
+		tmux attach-session -t $fbname
+	else
+		create_new $fbname $1
+	fi
+}
+
+function create_new {
+	tmux new-session -s $fbname \; \
+	        send-keys 'vim ' "$2" C-m \; \
+	        split-window -h -p 20 \;	\
+		select-pane -L \;	
+} 
+
+
+if `git rev-parse --show-toplevel` ; then
+	start_tmux `git rev-parse --show-toplevel`
+else
+	start_tmux $1
+fi
