95c583456aabf5093797a1293421fc305a98821b
Filippo <github@filippo.pw>
2017-10-21 22:53:22 +0200

    Update init_settings.sh

--- init_settings.sh
+++ init_settings.sh
@@ -1,8 +1,25 @@
 #! /bin/sh
+echo 'export TERM=xterm-256color' >> ~/.bashrc
 cd ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 wget https://raw.githubusercontent.com/Filo01/system_settings/master/.vimrc
 vim +PluginInstall +qall
-
 ~/.vim/bundle/YouCompleteMe/install.py
+
+wget https://raw.githubusercontent.com/Filo01/system_settings/master/.tmux.conf > ~/.tmux.conf
+wget https://raw.githubusercontent.com/Filo01/system_settings/master/.vimrc > ~/.vimrc
+sudo pip install virtualenvwrapper
+export WORKON_HOME=~/Envs
+source /usr/local/bin/virtualenvwrapper.sh
+export PROJECT_HOME=~/PythonProjects
+echo "pip install pylint" >> $WORKON_HOME/postmkproject
+echo "pip install autopep8" >> $WORKON_HOME/postmkproject
+echo "if [ -n \"'\$VIRTUAL_ENV\" ]; then
+    source \$VIRTUAL_ENV/bin/activate;
+    fi" >> ~/.bashrc
+echo "if [[ -n \"\$TMUX\" ]]; then
+    tmux set-environment VIRTUAL_ENV \$VIRTUAL_ENV
+    fi" >> ~/.tmux.conf
+
+git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
+echo 'source ~/.autoenv/activate.sh' >> ~/.bashrc
